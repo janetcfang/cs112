2fd12bdf515339518a66dded4802087a8fe03c64
Janet Fang <janetcfang@gmail.com>
2016-09-10 11:47:01 -0700

    Create StringOperations.java

---
+++ examples/week2/StringOperations.java
@@ -0,0 +1,49 @@
+public class StringOperations{
+	
+	public static void main(String[] args){
+
+		//prints "he"
+		/*
+		String s1 = new String("hello");
+		String sub = s1.substring(0,2);
+		System.out.println(sub);
+		*/
+
+
+   		
+   		//print out goodbye, world
+		/*
+		String s1 = new String("hello, world");
+		String news1 = s1.replaceFirst("hello", "goodbye");
+		System.out.println(news1);
+		*/
+
+		
+		//prints goodbye, world
+		/*
+		String s1 = new String("hello, world");
+		s1 = s1.replaceFirst("hello", "goodbye");
+		System.out.println(s1);
+		*/
+
+
+
+		//equal/diff example
+		/*
+		String s1 = new String("hello, world");
+		String s2 = new String("hello, world");
+
+		if(s1.equals(s2)) {
+			System.out.println("EQUAL");
+		}
+		else{
+			System.out.println("DIFFERENT");
+		}
+		*/
+
+		
+
+	}
+
+
+}
