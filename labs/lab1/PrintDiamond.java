0ce3b014c4308b62338367f9e0f7e7bcfd68b981
Janet Fang <janetcfang@gmail.com>
2016-09-14 00:37:22 -0700

    Create PrintDiamond.java

---
+++ labs/lab1/PrintDiamond.java
@@ -0,0 +1,22 @@
+/* 
+
+	Assignment is to create a program that prints out a diamond shape like this:
+	   *
+    * *
+   * * *
+  * * * *
+   * * *
+    * *
+     *
+	@author Janet Fang
+
+*/
+public class PrintDiamond{
+
+	public static void main(String[] args) {
+
+	System.out.println("     *\n    * *\n   * * *\n  * * * *\n   * * *\n    * *\n     *\n");
+
+	}
+}
+
