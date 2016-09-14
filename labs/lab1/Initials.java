feaed467039115168720cd56fb53168c5012d26a
Janet Fang <janetcfang@gmail.com>
2016-09-14 00:36:47 -0700

    Create Initials.java

---
+++ labs/lab1/Initials.java
@@ -0,0 +1,30 @@
+/* 
+
+	Assignment is to create a program that prints out initials in block letters:
+
+	      JJ   FFFFFFF
+	      JJ   FF
+	      JJ   FF 
+	 JJ   JJ   FFFFF
+	 JJ   JJ   FF
+	 JJJJJJJ   FF
+
+	@author Janet Fang
+
+*/
+public class Initials{
+
+	public static void main(String[] args) {
+
+	System.out.println("     JJ   FFFFFF");
+	System.out.println("     JJ   FF");
+	System.out.println("     JJ   FF");
+	System.out.println("JJ   JJ   FFFFF");
+	System.out.println("JJ   JJ   FF");
+	System.out.println("JJJJJJJ   FF");
+
+	}
+}
+
+
+
