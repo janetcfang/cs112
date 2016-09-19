160f0ce6bb2ed0d319000706a518933e13237142
Janet Fang <janetcfang@gmail.com>
2016-09-18 17:42:37 -0700

    Create StudentScoresDriver.java

---
+++ examples/week4/StudentScoresDriver.java
@@ -0,0 +1,22 @@
+
+/**
+	A class to build a list of Students, display all student
+	information, and display the average student score.
+	
+**/
+public class StudentScoresDriver {
+
+	public static void main(String[] args) {
+
+		//use the StudentListBuilder to build a StudentList from the file students.txt
+		StudentListBuilder builder = new StudentListBuilder();
+		StudentList list = builder.buildStudentList("students.txt");
+
+		//print information about all Students
+		System.out.println(list);
+
+		//print the average score of all Students
+		System.out.println("Average score: " + list.calculateAverage());
+	}
+
+}
